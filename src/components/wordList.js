181fb0b295f91c5adadfc372c18460f12858d440
Marjorie Tolentino <marjt1@yahoo.com>
2020-06-27 10:46:02 -0700

    Construct new lists for syllables

--- src/components/wordList.js
+++ src/components/wordList.js
@@ -2,22 +2,43 @@ export const foodList = ['eggs', 'bread', 'fish', 'milk', 'cake', 'soda', 'banan
 'waffle', 'apple', 'garlic', 'cucumber', 'salad', 'chicken', 'burger', 'cheese', 'yogurt', 'ketchup', 'pizza', 'taco',
 'pasta', 'rice', 'carrot', 'broccoli', 'french fries', 'ice cream', 'sandwich', 'popcorn', 'spaghetti', 'beans'];
 
+export const foodListSyl = ['eggs', 'bread', 'fish', 'milk', 'cake', 'so-da', 'ba-nan-a', 'chips', 'su-shi',
+'waf-fle', 'ap-ple', 'gar-lic', 'cu-cum-ber', 'sal-ad', 'chick-en', 'bur-ger', 'cheese', 'yo-gurt', 'ketch-up', 'piz-za', 'ta-co',
+'pas-ta', 'rice', 'car-rot', 'broc-co-li', 'french fries', 'ice cream', 'sand-wich', 'pop-corn', 'spa-ghet-ti', 'beans'];
+
 export const sportsList = ['tennis', 'football', 'basketball', 'soccer', 'hockey', 'volleyball', 'track', 'goalie', 'cheer',
 'tag', 'catch', 'throw', 'karate', 'golf', 'bicycle', 'swim', 'frisbee', 'wrestling', 'dodgeball', 'helmet', 'ping pong',
 'jogging', 'diving', 'lacrosse', 'rugby', 'skating', 'surfing', 'water polo', 'dancing', 'jetski'];
+
+export const sportsListSyl = ['ten-nis', 'foot-ball', 'bas-ket-ball', 'soc-cer', 'hock-ey', 'vol-ley-ball', 'track', 'goal-ie', 'cheer',
+'tag', 'catch', 'throw', 'ka-ra-te', 'golf', 'bi-cy-cle', 'swim', 'fris-bee', 'wres-tling', 'dodge-ball', 'hel-met', 'ping pong',
+'jog-ging', 'div-ing', 'la-crosse', 'rug-by', 'skat-ing', 'surf-ing', 'wa-ter po-lo', 'danc-ing', 'jet-ski'];
 
 export const musicList = ['piano', 'guitar', 'drum', 'band', 'tuba', 'flute', 'clarinet', 'trumpet', 'choir', 'jazz', 'harp',
 'violin', 'banjo', 'organ', 'instrument', 'tempo', 'baritone', 'chord', 'concert', 'genre', 'harmony', 'bass', 'tune',
 'melody', 'opera', 'metal'];
 
-export const kidsList = ['cat', 'dog', 'ball', 'fun', 'moon', 'food', 'pool', 'water', 'tree', 'duck', 'car',
-'bee', 'book', 'bat', 'leaf', 'sheep', 'leg', 'hug', 'meet', 'jam', 'big', 'mop', 'buzz', 'evil', 'fart',
-'spoon', 'maze', 'oven', 'pig'];
+export const musicListSyl = ['pi-an-o', 'gui-tar', 'drum', 'band', 'tu-ba', 'flute', 'clar-i-net', 'trum-pet', 'choir', 'jazz', 'harp',
+'vi-o-lin', 'ban-jo', 'or-gan', 'in-stru-ment', 'tem-po', 'bar-i-tone', 'chord', 'con-cert', 'gen-re', 'har-mo-ny', 'bass', 'tune',
+'mel-o-dy', 'op-er-a', 'met-al'];
 
 export const animalsList = ['zoo', 'dog', 'fish', 'cat', 'zebra', 'giraffe', 'monkey', 'dinosaur', 'cow',
 'horse', 'tiger', 'chicken', 'duck', 'panda', 'rabbit', 'lion', 'frog', 'spider', 'sheep', 'snail',
 'kangaroo', 'camel', 'bird', 'cobra', 'octopus', 'pig', 'eagle', 'deer', 'shark', 'lobster', 'goat'];
 
+export const animalsListSyl = ['zoo', 'dog', 'fish', 'cat', 'ze-bra', 'gi-raffe', 'mon-key', 'di-no-saur', 'cow',
+'horse', 'ti-ger', 'chick-en', 'duck', 'pan-da', 'rab-bit', 'lion', 'frog', 'spi-der', 'sheep', 'snail',
+'kan-ga-roo', 'cam-el', 'bird', 'co-bra', 'oc-to-pus', 'pig', 'ea-gle', 'deer', 'shark', 'lob-ster', 'goat'];
+
 export const colorsList = ['rose red', 'yellow', 'orange', 'purple', 'aqua', 'turquoise', 'cream', 'magenta', 'peach', 
 'scarlet', 'lavender', 'ruby', 'baby blue', 'cerulean', 'violet', 'silver', 'eggplant', 'emerald', 'raspberry',
 'fuchsia', 'apricot', 'burgundy', 'chestnut', 'indigo', 'platinum', 'tangerine', 'titanium'];
+
+export const colorsListSyl = ['rose red', 'yel-low', 'or-ange', 'pur-ple', 'aq-ua', 'tur-quoise', 'cream', 'ma-gen-ta', 'peach', 
+'scar-let', 'lav-en-der', 'ru-by', 'baby blue', 'ce-ru-le-an', 'vi-o-let', 'sil-ver', 'egg-plant', 'em-er-ald', 'rasp-ber-ry',
+'fuch-sia', 'ap-ri-cot', 'bur-gun-dy', 'chest-nut', 'in-di-go', 'plat-i-num', 'tan-ge-rine', 'ti-ta-ni-um'];
+
+// Always 1 syllable for easy playing
+export const kidsList = ['cat', 'dog', 'ball', 'fun', 'moon', 'food', 'pool', 'water', 'tree', 'duck', 'car',
+'bee', 'book', 'bat', 'leaf', 'sheep', 'leg', 'hug', 'meet', 'jam', 'big', 'mop', 'buzz', 'evil', 'fart',
+'spoon', 'maze', 'oven', 'pig'];
